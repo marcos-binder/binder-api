44b8dc80d1b26596039caf15e91aec546a81bdb8
Marcos Vitor <marcosvitor1994@gmail.com>
2025-09-16 13:35:39 -0300

    Update docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from "@nestjs/config"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
 import { GoogleModule } from "../google/google.module"
-import { BrasilsegModule } from "../brasilseg/brasilseg.module"
 import { DocsCampanhaModule } from "../docs_campanha/docs-campanha.module"
 import googleConfig from "../config/google.config"
 
@@ -14,7 +13,6 @@ import googleConfig from "../config/google.config"
       load: [googleConfig],
     }),
     GoogleModule,
-    BrasilsegModule,
     DocsCampanhaModule, // Novo módulo adicionado
   ],
   controllers: [AppController],
